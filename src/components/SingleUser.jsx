4e13b3909a242842b770a2c08a4c76db93ad05a3
vivien leroux <vivienmichelesi@gmail.com>
2021-01-20 10:08:17 +0100

    réorganisation totale du code d'affichage, no-verify cause props validation

---
+++ src/components/SingleUser.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SingleUser.css';
+
+export default function SingleUser(props) {
+  return (
+    <div className="Card">
+      <h2 className="Title">
+        {props.firstname} {props.lastname}
+      </h2>
+      <h3>Disponibility: {props.disponibility}</h3>
+      <img className="ProfileImg" src={props.picture} alt={props.id} />
+      <h3>{props.description}</h3>
+    </div>
+  );
+}
