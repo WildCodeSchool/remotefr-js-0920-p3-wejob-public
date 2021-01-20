07375dad3e22d1d0bc444075fb64fbe6123f9508
vivien leroux <vivienmichelesi@gmail.com>
2021-01-20 10:06:20 +0100

    réorganisation totale du code d'affichage

--- BDD.sql
+++ BDD.sql
@@ -23,7 +23,13 @@ CREATE TABLE IF NOT EXISTS `activity_area` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table wordpress.activity_area : ~2 rows (environ)
+/*!40000 ALTER TABLE `activity_area` DISABLE KEYS */;
+INSERT INTO `activity_area` (`id`, `name`) VALUES
+	(1, 'logistic'),
+	(2, 'informatic'),
+	(3, 'education');
+/*!40000 ALTER TABLE `activity_area` ENABLE KEYS */;
 
 -- Listage de la structure de la table wordpress. job
 CREATE TABLE IF NOT EXISTS `job` (
@@ -32,7 +38,13 @@ CREATE TABLE IF NOT EXISTS `job` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table wordpress.job : ~2 rows (environ)
+/*!40000 ALTER TABLE `job` DISABLE KEYS */;
+INSERT INTO `job` (`id`, `name`) VALUES
+	(1, 'pilote'),
+	(2, 'conducteur engins'),
+	(3, 'comptable');
+/*!40000 ALTER TABLE `job` ENABLE KEYS */;
 
 -- Listage de la structure de la table wordpress. language
 CREATE TABLE IF NOT EXISTS `language` (
@@ -41,7 +53,14 @@ CREATE TABLE IF NOT EXISTS `language` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table wordpress.language : ~4 rows (environ)
+/*!40000 ALTER TABLE `language` DISABLE KEYS */;
+INSERT INTO `language` (`id`, `language`) VALUES
+	(1, 'French'),
+	(2, 'English'),
+	(3, 'German'),
+	(4, 'Polish');
+/*!40000 ALTER TABLE `language` ENABLE KEYS */;
 
 -- Listage de la structure de la table wordpress. sector_of_activity
 CREATE TABLE IF NOT EXISTS `sector_of_activity` (
@@ -50,7 +69,31 @@ CREATE TABLE IF NOT EXISTS `sector_of_activity` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table wordpress.sector_of_activity : ~21 rows (environ)
+/*!40000 ALTER TABLE `sector_of_activity` DISABLE KEYS */;
+INSERT INTO `sector_of_activity` (`id`, `name`) VALUES
+	(1, 'Aéronautique'),
+	(2, 'Agroalimentaire – vins & spiritueux'),
+	(3, 'Automobile : machines et équipements'),
+	(4, 'Banque – assurance'),
+	(5, 'Bois – papier – carton – imprimerie, plastique, caoutchouc'),
+	(6, 'BTP – matériaux de construction'),
+	(7, 'Chimie – parachimie'),
+	(8, 'Commerce – négoce – distribution'),
+	(9, 'Economie Sociale et Solidaire'),
+	(10, 'Edition – communication – multimédia'),
+	(11, 'Electronique – électricité'),
+	(12, 'Etudes et conseils'),
+	(13, 'Industrie pharmaceutique – biotechnologies'),
+	(14, 'Informatique – télécoms'),
+	(15, 'Métallurgie – travail du métal'),
+	(16, 'Public : éducation, justice, armée…'),
+	(17, 'Santé – service à la personne'),
+	(18, 'Textile – habillement – chaussure'),
+	(19, 'Transport – logistique'),
+	(20, 'Autres services aux entreprises'),
+	(21, 'Autres');
+/*!40000 ALTER TABLE `sector_of_activity` ENABLE KEYS */;
 
 -- Listage de la structure de la table wordpress. user
 CREATE TABLE IF NOT EXISTS `user` (
@@ -78,7 +121,12 @@ CREATE TABLE IF NOT EXISTS `user` (
   CONSTRAINT `FK_user_activity_area` FOREIGN KEY (`activity_area_id`) REFERENCES `activity_area` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table wordpress.user : ~2 rows (environ)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `lastname`, `firstname`, `description`, `diploma`, `activity_area_id`, `awailability`, `mobility`, `years_of_experiment`, `password`, `mail`, `statut`, `create_at`, `update_at`, `open_to_formation`, `cv`, `linkedin`, `youtube`, `picture`) VALUES
+	(1, 'Wagner', 'Christian', 'lorem ipsum', NULL, 1, 0, 1000, 10, '1234', 'fra@hotmail.fr', 'online', '2020-12-30 12:12:12', '2021-01-18 10:15:40', b'0', NULL, NULL, NULL, NULL),
+	(2, 'Newman', 'Jack', 'lorem ipsum', 'ingenier', 3, 3, 50, 25, '4567', 'zer@umail.com', 'en attente de validation', '2019-09-20 09:09:09', '2021-01-18 10:15:41', b'1', NULL, NULL, NULL, NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 -- Listage de la structure de la table wordpress. user_job
 CREATE TABLE IF NOT EXISTS `user_job` (
@@ -90,7 +138,12 @@ CREATE TABLE IF NOT EXISTS `user_job` (
   CONSTRAINT `FK_user_job_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table wordpress.user_job : ~2 rows (environ)
+/*!40000 ALTER TABLE `user_job` DISABLE KEYS */;
+INSERT INTO `user_job` (`job_id`, `user_id`) VALUES
+	(1, 1),
+	(2, 2);
+/*!40000 ALTER TABLE `user_job` ENABLE KEYS */;
 
 -- Listage de la structure de la table wordpress. user_language
 CREATE TABLE IF NOT EXISTS `user_language` (
@@ -102,7 +155,13 @@ CREATE TABLE IF NOT EXISTS `user_language` (
   CONSTRAINT `FK_user_language_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table wordpress.user_language : ~2 rows (environ)
+/*!40000 ALTER TABLE `user_language` DISABLE KEYS */;
+INSERT INTO `user_language` (`user_id`, `language_id`) VALUES
+	(1, 1),
+	(1, 2),
+	(2, 1);
+/*!40000 ALTER TABLE `user_language` ENABLE KEYS */;
 
 -- Listage de la structure de la table wordpress. user_sector_of_activity
 CREATE TABLE IF NOT EXISTS `user_sector_of_activity` (
@@ -114,7 +173,12 @@ CREATE TABLE IF NOT EXISTS `user_sector_of_activity` (
   CONSTRAINT `FK_user_skill_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table wordpress.user_sector_of_activity : ~2 rows (environ)
+/*!40000 ALTER TABLE `user_sector_of_activity` DISABLE KEYS */;
+INSERT INTO `user_sector_of_activity` (`user_id`, `sector_of_activity_id`) VALUES
+	(1, 3),
+	(2, 4);
+/*!40000 ALTER TABLE `user_sector_of_activity` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,16 @@
 import React from 'react';
 import './App.css';
 import Public from './components/Public';
-import Candidat from './components/Candidat';
-
-const candidat = [
-  {
-    id: 0,
-    firstname: 'Christian',
-    lastname: 'Wagner',
-    job: 'conducteur de poids lourds',
-    qualification: null,
-    sectorOfActivity: 'logistic',
-    yearsOfExperiment: 10,
-    disponibility: 'as soon as possible',
-    mobility: 'France',
-    language: ['french', 'english'],
-    picture: 'https://via.placeholder.com/150',
-    description:
-      'Je suis conducteur de poids lours et je cherche un travail dans toute la france',
-  },
-  {
-    id: 1,
-    firstname: 'Jack',
-    lastname: 'Newman',
-    job: 'expert comptable',
-    qualification: 'Bac +5',
-    sectorOfActivity: 'finance',
-    yearsOfExperiment: 25,
-    disponibility: '3 months',
-    mobility: 'Rhone-Alpes',
-    language: 'french' /*"english", 'german',*/,
-    picture: 'https://via.placeholder.com/150',
-    description: 'lorem imsum dolor blabla',
-  },
-  {
-    id: 2,
-    firstname: 'Melanie',
-    lastname: 'Douglas',
-    job: 'caissière',
-    qualification: null,
-    sectorOfActivity: null,
-    yearsOfExperiment: 1,
-    disponibility: 'as soon as possible',
-    mobility: 'Bordeaux',
-    language: 'french',
-    picture: 'https://via.placeholder.com/150',
-    description: 'lorem imsum dolor blabla',
-  },
-  {
-    id: 3,
-    firstname: 'Edna',
-    lastname: 'Hale',
-    job: 'Chimiste',
-    qualification: 'Bac +2 IUT de Chimie' /*"Bac +5 Cpe Lyon",*/,
-    sectorOfActivity: 'Industry',
-    yearsOfExperiment: 0,
-    disponibility: '4 weeks',
-    mobility: 'Aix en Provence' /*"Region Nord",*/,
-    language: 'french' /*"english",*/,
-    picture: 'https://via.placeholder.com/150',
-    description: 'lorem imsum dolor blabla',
-  },
-];
+import ListUsers from './components/ListUsers';
 
 function App() {
-  const listCard = candidat.map((single) => (
-    <Candidat
-      key={single.id}
-      firstname={single.firstname}
-      lastname={single.lastname}
-      disponibility={single.disponibility}
-      picture={single.picture}
-      description={single.description}
-    />
-  ));
   return (
     <div className="App">
       {/* <Header /> */}
       <Public />
-      <div className="ListCard">{listCard}</div>
+      <div className="ListCard">
+        <ListUsers />
+      </div>
       {/* <Footer /> */}
     </div>
   );
