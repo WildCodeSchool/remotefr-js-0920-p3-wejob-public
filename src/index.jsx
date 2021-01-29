f549f6313b63757ddedacec40ca1c4e19d6e9169
Benoît Hubert <benoithubert@gmail.com>
2021-01-29 12:27:45 +0100

    Affichage des erreurs

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-// import './index.css';
+import { ErrorBoundary } from 'react-error-boundary';
+import './index.css';
 import App from './App';
 
 const isProd = process.env.NODE_ENV === 'production';
 const basename = isProd ? window.location.pathname.replace(/\/?$/, '') : '/';
 
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <div className="ErrorFallback" role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.stack}</pre>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
-      <App />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+      >
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
