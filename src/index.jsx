429bda811352364a1c0b751dac08a1dde17d9171
Benoît Hubert <benoithubert@gmail.com>
2021-01-29 16:38:27 +0100

    Fix tout

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 import './index.css';
-import './ample-theme.css';
 import App from './App';
+
+const isProd = process.env.NODE_ENV === 'production';
+const basename = isProd ? window.location.pathname.replace(/\/?$/, '') : '/';
+
+function ErrorFallback({ error }) {
+  return (
+    <div className="ErrorFallback" role="alert">
+      <p>Something went wrong:</p>
+      <pre>{console.dir(error) || error.stack}</pre>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
+    <BrowserRouter basename={basename}>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
