29d59d51bed3df05cb4a594a82948c2a5aa0e159
vivien leroux <vivienmichelesi@gmail.com>
2021-01-30 23:53:50 +0100

    .env + début du AuthContext

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
-// import regeneratorRuntime from 'regenerator-runtime';
-import React from 'react';
+import regeneratorRuntime from 'regenerator-runtime';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 import './index.css';
 import App from './App';
+import AuthContext from './components/AuthContext';
 
 const isProd = process.env.NODE_ENV === 'production';
 const basename = isProd ? window.location.pathname.replace(/\/?$/, '') : '/';
@@ -18,12 +19,21 @@ function ErrorFallback({ error, resetErrorBoundary }) {
   );
 }
 
+const [user, setUser] = useState({
+  id: 1,
+  name: 'john Doe',
+  email: 'martin@ht.com',
+  phone: '0666666666',
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <App />
-      </ErrorBoundary>
+      <AuthContext.Provider value={{ user: user }}>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <App />
+        </ErrorBoundary>
+      </AuthContext.Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
